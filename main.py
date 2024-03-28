4b2ea3c667b261de750b646fb878bc977b135655
Sara Imdad <smohammadzai.bese19seecs@seecs.edu.pk>
2024-03-28 23:19:44 +0500

    Prepare base model completed

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = 'Data Ingestion Stage'
-
 try:
     logger.info(f'>>>>>> {STAGE_NAME} Started <<<<<<')
     obj = DataIngestionTrainingPipeline()
     obj.main()
-    logger.info(f'>>>>>> {STAGE_NAME} Completed \n\n<<<<<<')
+    logger.info(f'>>>>>> {STAGE_NAME} Completed <<<<<<\n\n')
 except Exception as e:
     logger.exception(e)
     raise e  
+
+STAGE_NAME = 'Preparing Base Model Stage'
+try:
+    logger.info(f'>>>>>> {STAGE_NAME} Started <<<<<<')
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f'>>>>>> {STAGE_NAME} Completed <<<<<<\n\n')
+except Exception as e:
+    logger.exception(e)
+    raise e 
--- src/cnnClassifier/pipeline/stage_02_prepare_base_model.py
+++ src/cnnClassifier/pipeline/stage_02_prepare_base_model.py
@@ -1,24 +1,24 @@
-from cnnClassifier.components.data_ingestion import DataIngestion
+from cnnClassifier.components.prepare_base_model import PrepareBaseModel
 from cnnClassifier.config.configuration import ConfigurationManager
 from cnnClassifier import logger
 
-STAGE_NAME = 'Data Ingestion Stage'
+STAGE_NAME = 'Preparing Base Model Stage'
 
-class DataIngestionTrainingPipeline:
+class PrepareBaseModelTrainingPipeline:
     def __init__(self) -> None:
         pass
 
     def main(self):
         config = ConfigurationManager()
-        data_ingestion_config = config.get_data_ingestion_config()
-        data_ingestion = DataIngestion(config=data_ingestion_config)
-        data_ingestion.download_file()
-        data_ingestion.extract_zip_file()
+        prepare_base_model_config = config.get_prepare_base_model_config()
+        prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+        prepare_base_model.get_base_model()
+        prepare_base_model.update_base_model()
 
 if __name__ == '__main__':
     try:
         logger.info(f'>>>>>> {STAGE_NAME} Started <<<<<<')
-        obj = DataIngestionTrainingPipeline()
+        obj = PrepareBaseModelTrainingPipeline()
         obj.main()
         logger.info(f'>>>>>> {STAGE_NAME} Completed <<<<<<\n\n')
     except Exception as e:
